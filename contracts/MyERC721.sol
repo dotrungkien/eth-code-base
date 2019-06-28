877e1b2ce80bc83292f31d74c6b7e70a52b68c51
DienNd <38159404+diennguyenduy@users.noreply.github.com>
2019-06-28 13:51:15 +0700

    change function's accessibility

--- contracts/MyERC721.sol
+++ contracts/MyERC721.sol
@@ -7,7 +7,7 @@ contract MyERC721 is ERC721, ERC721Metadata {
 
   constructor() public ERC721Metadata("My ERC721 Token", "MyERC721") {}
 
-  function mintUniqueTokenTo(address _to, uint256 _tokenId) public {
+  function mintUniqueTokenTo(address _to, uint256 _tokenId) external {
     super._mint(_to, _tokenId);
     super._setTokenURI(_tokenId, "My Token URI");
   }
