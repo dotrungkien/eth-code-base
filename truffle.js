8e196a3e52bafdb8689c8bbf60a4c70bf28aa62a
Do Trung Kien <trungkien.keio@gmail.com>
2019-07-01 14:33:49 +0700

    add test script #6

--- truffle.js
+++ truffle.js
@@ -4,6 +4,11 @@ module.exports = {
       host: '127.0.0.1',
       port: 8545,
       network_id: '*'
+    },
+    ganache: {
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: '*'
     }
   },
 
