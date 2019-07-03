621fa0ed6066fa07245a0550934103437e06d659
Do Trung Kien <trungkien.keio@gmail.com>
2019-07-03 10:14:41 +0700

    add solidity-coverage #21

--- truffle.js
+++ truffle.js
@@ -9,7 +9,14 @@ module.exports = {
       host: '127.0.0.1',
       port: 8545,
       network_id: '*'
-    }
+    },
+    coverage: {
+      host: 'localhost',
+      network_id: '*',
+      port: 8555,
+      gas: 0xfffffffffff,
+      gasPrice: 0x01,
+    },
   },
 
   mocha: {
