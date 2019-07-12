ab3eda995cf9a95ff75bbbf5836ba2c5b148eff9
nguyen duy dien <nguyenduydien1311999@gmail.com>
2019-07-12 15:49:05 +0700

    create ERC721_address file to ./src when migrate contract

--- migrations/3_deploy_erc721.js
+++ migrations/3_deploy_erc721.js
@@ -1,5 +1,12 @@
+const fs = require('fs');
+
 const MyERC721 = artifacts.require('MyERC721');
 
 module.exports = function(deployer) {
-  deployer.deploy(MyERC721);
+  deployer.deploy(MyERC721).then((detail) => {
+    address = '{' + '"address":' + '"' + detail.constructor.address + '"' + '}';
+    fs.writeFile('./client/src/ERC721_Contract_address.json', address, 'utf8', (err) => {
+      console.log(detail.constructor.address);
+    });
+  });
 };
