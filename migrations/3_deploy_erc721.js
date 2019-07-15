902846b856549ba5bc630f4b5ba800c9851d67a9
nguyen duy dien <nguyenduydien1311999@gmail.com>
2019-07-15 13:04:15 +0700

    write address of deployed contract to json file

--- migrations/3_deploy_erc721.js
+++ migrations/3_deploy_erc721.js
@@ -4,7 +4,7 @@ const MyERC721 = artifacts.require('MyERC721');
 
 module.exports = function(deployer) {
   deployer.deploy(MyERC721).then((detail) => {
-    address = '{' + '"address":' + '"' + detail.constructor.address + '"' + '}';
+    address = '{' + '\n' + '"address":' + '"' + detail.constructor.address + '"' + '\n' + '}';
     fs.writeFile('./client/src/ERC721_Contract_address.json', address, 'utf8', (err) => {
       console.log(detail.constructor.address);
     });
